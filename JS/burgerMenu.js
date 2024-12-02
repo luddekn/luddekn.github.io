c95cb01284bddfda3e22866ef3e85d586ff8168c
Ludvik Kristoffersen <70239345+luddekn@users.noreply.github.com>
2024-12-03 00:20:41 +0100

    Update burgerMenu.js

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const burgerMenu = document.getElementById('burger-menu');
 	const headerLinks = document.getElementById('header-links');
-
 	burgerMenu.addEventListener('click', () => {
 		headerLinks.classList.toggle('hidden');
 	});
+
+	document.addEventListener('click', (e) => {
+		if (!burgerMenu.contains(e.target) && !headerLinks.contains(e.target)) {
+			headerLinks.classList.add('hidden');
+		}
+	});
 });
+
 
